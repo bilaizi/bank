e2a15e6d2340fea440eafdf6a348646201aa4360
bilaizi <2512075115@qq.com>
2018-04-13 16:00:41 +0800

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "menu.h"
+#include "person.h"
+
+void menu() {
+    printf("1 用户注册\n");
+    printf("1 用户登录\n");
+    printf("1 用户退出\n");
+     printf("1 \n");
+    return 0;
+}
+
+void do_register(int len) {
+    if (length)
+    printf("1 please input your username:\n");
+    scanf("1 please input your username:\n");
+    printf("1 用户登录\n");
+    printf("1 用户退出\n");
+     printf("1 \n");
+    return 0;
+}
