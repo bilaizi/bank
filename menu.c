db5ef8229e9def1801faa4e3dc38297859fb978c
bilaizi <2512075115@qq.com>
2018-04-13 16:17:19 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -5,11 +5,32 @@
 #include "person.h"
 
 void menu() {
-    printf("1 用户注册\n");
-    printf("2 用户登录\n");
-    printf("3 用户退出\n");
-     printf("1 \n");
-    return 0;
+        printf("1 用户注册：\n");
+        printf("2 用户登录：\n");
+        printf("3 用户退出：\n");
+        int select;
+        printf("请输入你的选择：\n");
+        scanf("d%",&select);
+        switch(select) {
+                case 1:;
+                case 2: do_register(int len);
+                case 3:
+                   
+        }
+        return;
+}
+
+void do_login(int len) {
+        if (0 == len)
+                array[len].id =10001;
+        else
+                array[len].id = array[len].id + 1;
+        printf("please input your username:\n");
+        scanf("s%",&array[len].username);
+        printf("please input your password:\n");
+        scanf("s%",&array[len].password);
+        array[len++].balance = 0;
+        return len;
 }
 
 void do_register(int len) {
