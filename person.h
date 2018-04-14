e74fc1aa7e7cf7fc949b4907595eb9f6596e3b45
bilaizi <2512075115@qq.com>
2018-04-14 17:07:43 +0800

    Update person.h

--- person.h
+++ person.h
@@ -5,7 +5,10 @@ typedef struct person {
 	char username[20];
 	char password[20];
 	float balance;
-} person,*pperson;
+} person;
 person array[100];
 void function(int i, int len);
+void do_deposit(int i, int len);
+void do_withdraw(int i, int len);
+void do_transfer(int i, int len);
 #endif
