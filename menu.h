258afc375e849ec65f1e1b68e1f9740aa33b2210
bilaizi <2512075115@qq.com>
2018-04-14 17:09:16 +0800

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,8 +1,9 @@
 #ifndef _MENU_H
 #define _MENU_H
+int read_file();
+void write_file(int len);
 void menu(int len);
 void do_login(int len);
 int do_register(int len);
-void write_file(int len);
 void show(int len);
 #endif
