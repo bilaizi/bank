a514ab4af48b81373010f7bf16e1534925bb9919
bilaizi <2512075115@qq.com>
2018-04-14 11:08:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "menu.h"
 #include "person.h"
 
 int main(){
-    
+    int len = read_file();
+    menu(len);
     return 0;
 }
-read_file();
-write_file();
