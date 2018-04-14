e8eb19e6fdc3d232a76b1878c67d10ebde8a5287
bilaizi <2512075115@qq.com>
2018-04-14 09:43:50 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -6,8 +6,8 @@
 
 void menu() {
         printf("*************************\n");
-	printf("*1	register	*\n");
-	printf("*2 	login   	*\n");
+	printf("*1	login		*\n");
+	printf("*2 	register   	*\n");
 	printf("*3 	exit		*\n");
 	printf("*************************\n");
         int select;
